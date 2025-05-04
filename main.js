64b486be10ceddc818c59dae0edf362cd3942d9d
Ben <benjamin.w.fyfe@gmail.com>
2025-05-04 13:53:02 +0100

    build: reset repo

--- main.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-    $('.ham').click(function() {
-        $('.ham').toggleClass('active');
-        $('.hamburger-menu').css('display', 'flex');
-        $('.hamburger-menu').toggleClass('active-menu');
-        $('.hamburger-menu').toggleClass('closed');
-    });
-});
