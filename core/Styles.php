6aae393492b4b246cfc7ed5555225ac8f4c57cec
Ben <benjamin.w.fyfe@gmail.com>
2025-05-04 20:18:06 +0100

    feat: add typography style

--- core/Styles.php
+++ core/Styles.php
@@ -11,16 +11,26 @@ class Styles
     public static function init()
     {
         add_action('wp_enqueue_scripts', [__CLASS__, 'enqueueGlobalStyle']);
+        add_action('wp_enqueue_scripts', [__CLASS__, 'enqueueTypography']);
     }
 
     public static function enqueueGlobalStyle()
     {
-        error_log('enqueueGlobalStyle called'); // More reliable than echo
         wp_enqueue_style(
             'global-style',
             THEME_BASE . '/css/global.css',
             [],
             filemtime(THEME_PATH . '/css/global.css')
+        );
+    }
+
+    public static function enqueueTypography()
+    {
+        wp_enqueue_style(
+            'typography-style',
+            THEME_BASE . '/css/typography.css',
+            [],
+            filemtime(THEME_PATH . '/css/typography.css')
         );
     }
 }
